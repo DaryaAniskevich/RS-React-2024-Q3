65289ac4b25fe537f801c5aa10390bba6cca7759
DaryaAniskevich <darya.aniskevich@mail.ru>
2024-07-02 19:44:09 +0300

    feat: add eslint and prettier configs

---
+++ class-components/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.app.json',
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-refresh', 'react-compiler'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/prefer-stateless-function': 'off',
+    'react-compiler/react-compiler': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
+};
