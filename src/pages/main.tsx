f0ba02d0df262118999f7984e7248467e95dc414
DaryaAniskevich <darya.aniskevich@mail.ru>
2024-07-19 19:54:12 +0300

    test: remove error boundary tests

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Outlet, useSearchParams } from 'react-router-dom';
 import Search from '../components/main/Search';
 import Results from '../components/main/Results';
@@ -7,27 +7,14 @@ import { ResultProvider } from '../context/resultContext';
 function Main() {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const [isError, setIsError] = useState(false);
-
-  const addError = () => {
-    setIsError(true);
-  };
-
   useEffect(() => {
     if (!searchParams.get('page')) {
       setSearchParams({ page: '1' });
     }
   }, [searchParams]);
 
-  if (isError) {
-    throw new Error('Something went wrong');
-  }
-
   return (
     <main className="container">
-      <button type="button" onClick={addError} className="button-boundary" aria-label="Check Error">
-        Check Error Boundary
-      </button>
       <div className="main">
         <div className="content">
           <ResultProvider>
